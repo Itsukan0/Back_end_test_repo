57fe01f61a9b3aa88b2b1426c2b8a77794d68487
RAKOTOMALALA Hassim Mazahere <hassimmazahere@gmail.com>
2025-04-03 16:26:13 +0200

    fix (pytest): remove pytest file as they should be in another branch

--- tests/test_main.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-from fastapi_app import app
-
-client = TestClient(app)
-
-def test_read_main():
-    response = client.get("/api/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Bienvenue sur l'API de la bibliothèque"}
