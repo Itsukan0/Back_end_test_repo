c238d50678056f3b1f28815a0f95e6f2ab7c699e
RAKOTOMALALA Hassim Mazahere <hassimmazahere@gmail.com>
2025-04-16 00:47:59 +0200

    feat (routes): add patch, working routes

--- fastapi_app.py
+++ fastapi_app.py
@@ -1,7 +1,16 @@
 import os
-import django
-from fastapi import FastAPI, APIRouter, HTTPException
 from typing import List
+
+from asgiref.sync import sync_to_async
+
+from fastapi import FastAPI, APIRouter, HTTPException
+from fastapi import status
+
+import django
+from django.shortcuts import get_object_or_404
+from django.core.management import call_command
+from django.db import connections
+from django.core.exceptions import ImproperlyConfigured
 
 # Initialiser Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'name.settings')
@@ -9,7 +18,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'name.settings')
 django.setup()
 
 from books.models import Book as DjangoBook  # Importer ton modèle Django
-from books.schemas import BookCreate, BookOut
+from books.schemas import BookCreate, BookOut, BookUpdate
 
 # Créer l'instance FastAPI
 app = FastAPI()
@@ -22,14 +31,14 @@ api_router = APIRouter()
 def read_root():
     return {"message": "Bienvenue sur l'API de la bibliothèque"}
 
-# Route GET pour récupérer tous les livres
+# # Route GET pour récupérer tous les livres
 @api_router.get("/books/", response_model=List[BookOut])
 def get_books():
     books = DjangoBook.objects.all()
     return books
 
-# Route GET pour récupérer un livre par son ID
-@api_router.get("/books/{book_id}", response_model=BookOut)
+# # Route GET pour récupérer un livre par son ID
+@api_router.get("/book/{book_id}", response_model=BookOut)
 def get_book(book_id: int):
     try:
         book = DjangoBook.objects.get(id=book_id)
@@ -37,8 +46,8 @@ def get_book(book_id: int):
     except DjangoBook.DoesNotExist:
         raise HTTPException(status_code=404, detail="Book not found")
 
-# Route POST pour créer un livre
-@api_router.post("/books/", response_model=BookOut)
+# # Route POST pour créer un livre
+@api_router.post("/book/", response_model=BookOut, status_code=status.HTTP_201_CREATED)
 def create_book(book: BookCreate):
     new_book = DjangoBook.objects.create(
         title=book.title,
@@ -52,7 +61,7 @@ def create_book(book: BookCreate):
     return new_book
 
 # Route PUT pour mettre à jour un livre
-@api_router.put("/books/{book_id}", response_model=BookOut)
+@api_router.put("/book/{book_id}", response_model=BookOut)
 def update_book(book_id: int, book: BookCreate):
     try:
         existing_book = DjangoBook.objects.get(id=book_id)
@@ -68,8 +77,20 @@ def update_book(book_id: int, book: BookCreate):
     except DjangoBook.DoesNotExist:
         raise HTTPException(status_code=404, detail="Book not found")
 
+@api_router.patch("/book/{book_id}")
+def update_book(book_id: int, book_update: BookUpdate):
+    try:
+        book = DjangoBook.objects.get(id=book_id)
+    except DjangoBook.DoesNotExist:
+        raise HTTPException(status_code=404, detail="Book not found")
+    update_data = book_update.model_dump(exclude_unset=True)
+    for key, value in update_data.items():
+        setattr(book, key, value)
+    book.save()
+    return book
+
 # Route DELETE pour supprimer un livre
-@api_router.delete("/books/{book_id}")
+@api_router.delete("/book/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_book(book_id: int):
     try:
         book = DjangoBook.objects.get(id=book_id)
