9414055f7188a31c14f9090508a997fb95b2a855
RAKOTOMALALA Hassim Mazahere <hassimmazahere@gmail.com>
2025-04-16 00:47:38 +0200

    fix (conftest): add global fixtures in the conftest.py file

--- conftest.py
+++ conftest.py
@@ -1,17 +1,26 @@
 import pytest
-from django.conf import settings
-import os
 
-@pytest.fixture(scope="session", autouse=True)
-def django_db_setup():
-    """Configure Django pour utiliser SQLite en mémoire pendant les tests."""
+from django.core.exceptions import ImproperlyConfigured
+from django.db import connections
+from django.core.management import call_command
 
-    settings.DATABASES["default"] = {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": ":memory:",  # Utilise SQLite en mémoire
-        # "TIME_ZONE": None,
-        # "CONN_HEALTH_CHECKS": False,
-        # "CONN_MAX_AGE": 0,
-        # 'OPTIONS': {},
-        # "AUTOCOMMIT": True,
-    }
+from fastapi_app import app
+from fastapi.testclient import TestClient
+
+@pytest.fixture(autouse=True, scope="function")
+def setup_db():
+    connection = connections['default']
+    try:
+        connection.connect()  # Connexion à la base de données
+        call_command('makemigrations', interactive=False)
+        call_command('migrate', interactive=False)  # Applique les migrations sans demander d'entrée
+    except ImproperlyConfigured:
+        # Si la configuration est incorrecte (par exemple, problème de base de données)
+        raise Exception("Django settings are not properly configured")
+    yield
+    # Si tu as besoin de faire un nettoyage après chaque test, tu peux le faire ici
+
+@pytest.fixture()
+def client():
+    """Fixture pour le client de test FastAPI."""
+    return TestClient(app)
